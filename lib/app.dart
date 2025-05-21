864f59be5151654a7b4a3b32c66710fadced73e0
Majdi Aribi <majdi.developer@gmail.com>
2025-05-21 06:17:00 +0100

    add l10n, app secrets, and update .gitignore

---
+++ lib/app.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        ),
+        localizationsDelegates: const [
+          AppLocalizations.delegate, // Add this line
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: [
+          Locale('en'),
+          Locale('fr'),
+        ],
+        home: Scaffold(
+          appBar: AppBar(
+            title: const Text('Flutter Demo Home Page'),
+          ),
+          body: const Center(
+            child: Text('Hello World'),
+          ),
+        ));
+  }
+}
